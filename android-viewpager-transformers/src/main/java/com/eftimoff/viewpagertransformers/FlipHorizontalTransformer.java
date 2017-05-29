83e52f6d4e6ff999a4d8a56e436bb93471032468
hshahdoost <shahdoost.hossein@outlook.com>
2017-05-29 11:57:58 +0430

    Bug #14, setAlpha changed to setVisibility in order to block the functionality of the hidden page

--- android-viewpager-transformers/src/main/java/com/eftimoff/viewpagertransformers/FlipHorizontalTransformer.java
+++ android-viewpager-transformers/src/main/java/com/eftimoff/viewpagertransformers/FlipHorizontalTransformer.java
@@ -24,7 +24,7 @@ public class FlipHorizontalTransformer extends BaseTransformer {
 	protected void onTransform(View view, float position) {
 		final float rotation = 180f * position;
 
-		view.setAlpha(rotation > 90f || rotation < -90f ? 0 : 1);
+		view.setVisibility(rotation > 90f || rotation < -90f ? View.INVISIBLE : View.VISIBLE);
 		view.setPivotX(view.getWidth() * 0.5f);
 		view.setPivotY(view.getHeight() * 0.5f);
 		view.setRotationY(rotation);
